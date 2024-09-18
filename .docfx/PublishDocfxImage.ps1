39abe7c79509bcc63c86736a0b002b6eff18c30a
Michael Mortensen <michael@geekle.io>
2024-09-18 17:53:49 +0200

    housekeeping/updated to be contemporary (#1)

--- .docfx/PublishDocfxImage.ps1
+++ .docfx/PublishDocfxImage.ps1
@@ -1,3 +1,3 @@
-$version = minver -i
-docker tag sharedkernel-docfx:$version yourbranding/classlibrary1:$version
+$version = minver -i -t v -v w
+docker tag classlibrary1-docfx:$version yourbranding/classlibrary1:$version
 docker push yourbranding/classlibrary1:$version
